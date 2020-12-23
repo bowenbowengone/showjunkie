7b9c500c59a52c639cdfe9c44816e0c84c5b7831
Jennifer Bowen <jrog814@gmail.com>
2020-12-23 12:00:23 -0500

    Add Show Form

--- components/AddShowForm.js
+++ components/AddShowForm.js
@@ -1,141 +1,144 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, ScrollView, Button, TouchableHighlight, FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  Button,
+  TouchableHighlight,
+  FlatList,
+} from 'react-native'
+import { ListItem } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import DateTimePicker from '@react-native-community/datetimepicker'
 import { SHOWLIST } from '../shared/showlist'
-
-
 class AddShowForm extends Component {
-    
-        state = {
-            artist: '',
-            date: new Date(),
-            venue: '',
-            city: '',
-            state: '',
-            key: '',
-            showCalendar: false
-        }
+  state = {
+    artist: '',
+    date: new Date(),
+    venue: '',
+    city: '',
+    state: '',
+    showList: SHOWLIST,
+    showCalendar: false,
+    showMore: false
+  }
 
-    
-    submitHandler = () => {
-        const newShowlist = SHOWLIST.concat([this.state])
-        this.state.key = Object.keys(newShowlist).length - 1;
-            console.log(newShowlist);
-    };
+  submitHandler = () => {
+    const { artist, date, venue, city, state } = this.state
+    const newShow = { artist, date: date.toDateString(), venue, city, state, key: this.state.showList.length }
+    const newShowlist = this.state.showList.concat(newShow)
+    // You'll want to do some data validation because as the code sits, if the venue is blank, it will cause an error
+    this.setState({ showList: newShowlist })
+    this.resetForm()
+  }
 
-    resetForm() {
-        this.setState({
-            artist: '',
-            date: new Date(),
-            venue: '',
-            city: '',
-            state: '',
-            key: '',
-            showCalendar: false
-        });
+  resetForm() {
+    this.setState({
+      artist: '',
+      date: new Date(),
+      venue: '',
+      city: '',
+      state: '',
+      key: '',
+      showCalendar: false,
+      showMore: false
+    })
+  }
+  render() {
+    const renderShow = ({ item }) => {
+      return <ListItem title={item.artist} subtitle={item.venue}  />
     }
-
-    render() {
-        const renderShow = ({item}) => {
-            return (
-                
-                    <ListItem 
-                        title={item.artist}
-                        subtitle={item.venue}
-                    />
-
-                
-            );
-        }
-
-        return (
-            <ScrollView>
-                <View style={styles.formRow}>
-                    <Text style={styles.formLabel}>Artist:</Text>
-                    <TextInput 
-                        placeholder='artist'
-                        onChangeText={(value) => this.setState({artist: value})}
-                        value={this.state.artist}
-                    />
-                </View>
-                <View style={styles.formRow}>
-                    <Text style={styles.formLabel}>Venue:</Text>
-                    <TextInput 
-                        placeholder='venue'
-                        onChangeText={(value) => this.setState({venue: value})}
-                        value={this.state.venue}
-                    />
-                </View>
-                <View style={styles.formRow}>
-                    <Text style={styles.formLabel}>City:</Text>
-                    <TextInput 
-                        placeholder='city'
-                        onChangeText={(value) => this.setState({city: value})}
-                        value={this.state.city}
-                    />
-                </View>
-                <View style={styles.formRow}>
-                    <Text style={styles.formLabel}>State:</Text>
-                    <TextInput 
-                        placeholder='state'
-                        onChangeText={(value) => this.setState({state: value})}
-                        value={this.state.state}
-                    />
-                </View>
-                <View style={styles.formRow}>
-                    <Text style={styles.formLabel}>Date</Text>
-                    <Button
-                        onPress={() =>
-                            this.setState({showCalendar: !this.state.showCalendar})
-                        }
-                        title={this.state.date.toLocaleDateString('en-US')}
-                        color='black'
-                        accessibilityLabel='select show date'
-                    />
-                </View>
-                {this.state.showCalendar && (
-                    <DateTimePicker
-                        value={this.state.date}
-                        mode={'date'}
-                        display='default'
-                        onChange={(event, selectedDate) => {
-                            selectedDate && this.setState({date: selectedDate, showCalendar: false});
-                        }}
-                        style={styles.formItem}
-                    />
-                )}
-                <TouchableHighlight>
-                    <Button onPress={() => {this.submitHandler(); this.resetForm();}} title='add show' color='black'/>
-                </TouchableHighlight>
-                <Text>
-                    <FlatList 
-                        data={SHOWLIST}
-                        renderItem={renderShow}
-                        keyExtractor={item => item.key.toString()}
-                    />
-                </Text>
-            </ScrollView>
-            
-        );
-    }
+    return (
+      <ScrollView>
+        <View style={styles.formRow}>
+          <Text style={styles.formLabel}>Artist:</Text>
+          <TextInput
+            placeholder='artist'
+            onChangeText={(value) => this.setState({ artist: value })}
+            value={this.state.artist}
+          />
+        </View>
+        <View style={styles.formRow}>
+          <Text style={styles.formLabel}>Venue:</Text>
+          <TextInput
+            placeholder='venue'
+            onChangeText={(value) => this.setState({ venue: value })}
+            value={this.state.venue}
+          />
+        </View>
+        <View style={styles.formRow}>
+          <Text style={styles.formLabel}>City:</Text>
+          <TextInput
+            placeholder='city'
+            onChangeText={(value) => this.setState({ city: value })}
+            value={this.state.city}
+          />
+        </View>
+        <View style={styles.formRow}>
+          <Text style={styles.formLabel}>State:</Text>
+          <TextInput
+            placeholder='state'
+            onChangeText={(value) => this.setState({ state: value })}
+            value={this.state.state}
+          />
+        </View>
+        <View style={styles.formRow}>
+          <Text style={styles.formLabel}>Date</Text>
+          <Button
+            onPress={() => this.setState({ showCalendar: !this.state.showCalendar })}
+            title={this.state.date.toLocaleDateString('en-US')}
+            color='black'
+            accessibilityLabel='select show date'
+          />
+        </View>
+        {this.state.showCalendar && (
+          <DateTimePicker
+            value={this.state.date}
+            mode={'date'}
+            display='default'
+            onChange={(event, selectedDate) => {
+              selectedDate && this.setState({ date: selectedDate, showCalendar: false })
+            }}
+            style={styles.formItem}
+          />
+        )}
+        <TouchableHighlight>
+          <Button
+            onPress={() => {
+              this.submitHandler()
+              this.resetForm()
+            }}
+            title='add show'
+            color='black'
+          />
+        </TouchableHighlight>
+        <Text>
+          <FlatList
+            data={this.state.showList}
+            renderItem={renderShow}
+            keyExtractor={(item) => item.key.toString()}
+          />
+        </Text>
+      </ScrollView>
+    )
+  }
 }
-
 const styles = StyleSheet.create({
-    formRow: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-        flexDirection: 'row',
-        margin: 5
-    },
-    formLabel: {
-        fontSize: 16,
-        flex: 2
-    },
-    formItem: {
-        flex: 1
-    }
-});
-
-export default AddShowForm;
+  formRow: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    margin: 5,
+  },
+  formLabel: {
+    fontSize: 16,
+    flex: 2,
+  },
+  formItem: {
+    flex: 1,
+  },
+})
+export default AddShowForm
--- shared/showlist.js
+++ shared/showlist.js
@@ -1,5 +1,3 @@
 export const SHOWLIST = [
-    {artist: 'sam bush', venue: 'spinning jenny', city: 'Greer', state: 'SC', key: '0'},
-    {artist: 'bela fleck', venue: 'red rocks', city: 'Morrison', state: 'CO', key: '1'},
-    {artist: 'tedeschi trucks band', venue: 'fox theater', city: 'Atlanta', state: 'GA', key: '2'}
+    {artist: '', venue: '', city: '', state: '', key: '0'},
 ];
